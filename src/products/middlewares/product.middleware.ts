eb9df7137fb6988f3e3d7ef730c5e11d09ae27e5
JoaquinMarmol <joaquinmarmolnecochea@gmail.com>
2023-10-18 17:57:37 -0300

    product middleware

--- src/products/middlewares/product.middleware.ts
+++ src/products/middlewares/product.middleware.ts
@@ -1,27 +1,34 @@
-import { validate } from "class-validator";
-import { NextFunction, Request, Response } from "express";
-import { HttpResponse } from "../../shared/response/http.status.response";
-import { ProductDTO } from "../dto/product.dto";
+import { validate } from 'class-validator'
+import { NextFunction, Request, Response } from 'express'
+import { HttpResponse } from '../../shared/response/http.status.response'
+import { ProductDTO } from '../dto/product.dto'
 
 export class ProductMiddleware {
   constructor(
     private readonly httpResponse: HttpResponse = new HttpResponse()
   ) {}
   productValidator(req: Request, res: Response, next: NextFunction) {
-    const { productName, description, category, price } = req.body;
+    const {
+      productName,
+      lowDescription,
+      category,
+      price,
+      longDescription
+    } = req.body
 
-    const valid = new ProductDTO();
-    valid.productName = productName;
-    valid.description = description;
-    valid.category = category;
-    valid.price = price;
+    const valid = new ProductDTO()
+    valid.productName = productName
+    valid.LowDescription = lowDescription
+    valid.LongDescription = longDescription
+    valid.category = category
+    valid.price = price
 
     validate(valid).then((err) => {
       if (err.length > 0) {
-        return this.httpResponse.InternalServerError(res, err);
+        return this.httpResponse.InternalServerError(res, err)
       } else {
-        next();
+        next()
       }
-    });
+    })
   }
 }
