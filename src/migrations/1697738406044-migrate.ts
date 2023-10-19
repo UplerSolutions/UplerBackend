9557e8611815d77808f10a610a5ff4140b45ad90
Serafin Quesada <quesada.serafin03@gmail.com>
2023-10-19 15:05:14 -0300

    migration

--- src/migrations/1697738406044-migrate.ts
+++ src/migrations/1697738406044-migrate.ts
@@ -1,10 +1,9 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migrate1697725965530 implements MigrationInterface {
-    name = 'Migrate1697725965530'
+export class Migrate1697738406044 implements MigrationInterface {
+    name = 'Migrate1697738406044'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`product\` DROP COLUMN \`image\``);
         await queryRunner.query(`ALTER TABLE \`product\` DROP COLUMN \`diirect_link\``);
         await queryRunner.query(`ALTER TABLE \`product\` ADD \`direct_link\` varchar(255) NOT NULL`);
         await queryRunner.query(`ALTER TABLE \`product\` ADD \`image_url\` varchar(255) NOT NULL`);
@@ -14,7 +13,6 @@ export class Migrate1697725965530 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`product\` DROP COLUMN \`image_url\``);
         await queryRunner.query(`ALTER TABLE \`product\` DROP COLUMN \`direct_link\``);
         await queryRunner.query(`ALTER TABLE \`product\` ADD \`diirect_link\` varchar(255) NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`product\` ADD \`image\` varchar(255) NOT NULL`);
     }
 
 }
